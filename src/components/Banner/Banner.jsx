10d1d33b1e13ca1d7d652999f7bd7ad5948f4823
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-09-21 02:52:08 +0600

    banner desgin code added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -12,9 +12,16 @@ const interFont = Inter({
 
 const Banner = () => {
     return (
-        <div className="relative bg-[#880808] text-white h-screen flex justify-between items-center px-12">
+        <div className=" relative bg-[#880808] text-white flex flex-col justify-center items-center h-screen px-12">
+            <Image
+                src="/assets/banner/Ellipse-1.png"
+                className=' absolute top-0 -left-0'
+                alt="Breakfast 1"
+                width={400}
+                height={400}
+            />
             {/* Text Section */}
-            <div className="">
+            <div className=" static -top-44 left-0">
                 <h1 className={`${sanchezFont.className} text-4xl font-bold mb-1`}>BREAKFAST</h1>
                 <p className={`${interFont.className}`}>
                     Breakfast, often referred to as the ‘most important meal of the day’, provides
@@ -23,8 +30,45 @@ const Banner = () => {
                     Eating a healthy breakfast can improve concentration and performance, boost physical
                     endurance, and lower cholesterol levels.
                 </p>
+            </div>  
+            {/* banner small images */}
+            <div className="  -top-44 left-0 flex items-center justify-center">
+                <Image
+                    src="/assets/banner/food-1.png"
+                    alt="Breakfast 1"
+                    width={200}
+                    height={200}
+                    className=' h-[182.51px] w-[182.51px] rounded-full'
+                />
+                <Image
+                    src="/assets/banner/food-2.png"
+                    alt="Breakfast 2"
+                    width={200}
+                    height={200}
+                    className=' h-[182.51px] w-[182.51px] rounded-full'
+                />
+                <Image
+                    src="/assets/banner/food-3.png"
+                    alt="Breakfast 3"
+                    width={200}
+                    height={200}
+                    className=' h-[182.51px] w-[182.51px] rounded-full'
+                />
+                <Image
+                    src="/assets/banner/food-4.png"
+                    alt="Breakfast 3"
+                    width={200}
+                    height={200}
+                    className=' h-[182.51px] w-[182.51px] rounded-full'
+                />
             </div>
-
+            <Image
+                src="/assets/banner/Ellipse-2.png"
+                className=' bottom-0 right-0'
+                alt="Breakfast 1"
+                width={200}
+                height={200}
+            />
         </div>
     );
 };
